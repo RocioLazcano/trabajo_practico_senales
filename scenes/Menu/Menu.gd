fcdedb5bbfc79c56dedffabac6b003124201b1b7
RocioLazcano <80860184+RocioLazcano@users.noreply.github.com>
2021-04-20 23:29:19 -0300

    Punto_7

--- scenes/Menu/Menu.gd
+++ scenes/Menu/Menu.gd
@@ -3,6 +3,7 @@ extends Node2D
 const INSTANCES_TEXT = "Cantidad de instancias: "
 # Variable para contar cantidad de instancias
 var instancesCount = 0
+
 func count_new_instance():
 	if Input.is_action_just_pressed("left_click"):
 		instancesCount += 1
@@ -12,4 +13,7 @@ func _ready():
 	# Inicializa el texto de "CountLabel" usando el texto
 	# Constante "INSTANCES_TEXT" más el valor de "instancesCount" como Texto
 	$CountLabel.text = INSTANCES_TEXT + String(instancesCount)
+	$RedGenerator.connect("button_down" , self , "count_new_instance")
+	$VioletGenerator.connect("button_down" , self , "count_new_instance")
+
 	pass
