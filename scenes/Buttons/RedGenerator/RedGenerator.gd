1fdfb434da28f59ec8a8007960b1a66df5beaa6c
RocioLazcano <80860184+RocioLazcano@users.noreply.github.com>
2021-04-20 22:47:32 -0300

    punto_2

--- scenes/Buttons/RedGenerator/RedGenerator.gd
+++ scenes/Buttons/RedGenerator/RedGenerator.gd
@@ -1,16 +1,15 @@
 extends TextureButton
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
+func _generate_block():
+	var ins_rojo_chico = preload("res://scenes/Blocks/Red/BlockRed.tscn")
+	
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _on_RedGenerator_button_down():
+	_generate_block()
+	pass # Replace with function body.
--- scenes/Buttons/VioletGenerator/VioletGenerator.gd
+++ scenes/Buttons/VioletGenerator/VioletGenerator.gd
@@ -1,9 +1,7 @@
 extends TextureButton
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+
 
 
 # Called when the node enters the scene tree for the first time.
@@ -11,6 +9,7 @@ func _ready():
 	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+
+
+func _on_VioletGenerator_button_down():
+	pass # Replace with function body.
